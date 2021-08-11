fe182037f5da042c28f10493bdf864a85cf97c8b
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-10 19:55:56 -0700

    used cheerio to scrape navbar logo off website

--- scraper.js
+++ scraper.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const request = require('request');
+const cheerio = require('cheerio');
+const { log } = require('console');
+
+
+
+request('https://webscraper.io/test-sites/e-commerce/static', (e, res, html) => {
+    if (!e && res.statusCode === 200) {
+        const $ = cheerio.load(html);
+        const siteHeading = $('.navbar-brand');
+        console.log(siteHeading.html());
+    }
+});
+
+
