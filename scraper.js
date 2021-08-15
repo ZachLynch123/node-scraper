41b03f39c34faced30387458dcf8204ae055d36c
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-15 12:10:33 -0700

    scraped every link to music files

--- scraper.js
+++ scraper.js
@@ -7,8 +7,17 @@ const vgmUrl = 'https://www.vgmusic.com/music/console/nintendo/nes';
 
 
 
-
-
+got(vgmUrl)
+.then(res => {
+    const $ = cheerio.load(res.body);
+    $('a').each((index, link) => {
+        const href = link.attribs.href;
+        console.log(href);
+    });
+})
+.catch(e => {
+    console.log(e);
+})
 
 
 
