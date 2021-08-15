88fabef6a9cc9bfa4e32b2b9660b076544d759a6
Zach Lynch <zachary.lynch1337@gmail.com>
2021-08-15 12:19:17 -0700

    scraper grabs all unique mid files (no remixes or duplicates)

--- scraper.js
+++ scraper.js
@@ -10,7 +10,7 @@ const vgmUrl = 'https://www.vgmusic.com/music/console/nintendo/nes';
 got(vgmUrl)
 .then(res => {
     const $ = cheerio.load(res.body);
-    $('a').each((index, link) => {
+    $('a').filter(isMusic).filter(noRemixOrDuplicate).each((i, link) => {
         const href = link.attribs.href;
         console.log(href);
     });
@@ -19,6 +19,20 @@ got(vgmUrl)
     console.log(e);
 })
 
+
+const isMusic = (i, link) => {
+    // returns false if the link doesn't link to a .mid file
+    if (typeof link.attribs.href === 'undefined') {
+        return false
+    }
+    return link.attribs.href.includes('.mid');
+};
+
+const noRemixOrDuplicate = (i, link) => {
+    // Regex to see if song title hs paretheses
+    const parenRegex = /^((?!\().)*$/;
+    return parenRegex.test(link.children[0].data);
+}
 
 
 
